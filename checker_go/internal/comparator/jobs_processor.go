e3b975d1e7c6bce0a2f845f6553f9dcac5cb52cc
Ilyas Adygamov <adygamovilyas@gmail.com>
2022-11-14 17:20:42 +0300

    fix module types

--- checker_go/internal/comparator/jobs_processor.go
+++ checker_go/internal/comparator/jobs_processor.go
@@ -2,27 +2,30 @@ package comparator
 
 import (
 	"gorm.io/gorm"
+	"io/ioutil"
+	"log"
+	"time"
 )
 
 type AttemptsCheckJobs struct {
 	id                   int64 `gorm:"primaryKey"`
 	job                  string
-	attemptLhs           string
-	attemptRhs           string
-	scriptCheckingResult string
+	attemptLhs           int
+	attemptRhs           int
+	scriptCheckingResult float64
 	status               string
-	checkingStartTime    string
-	checkingEndTime      string
+	checkingStartTime    time.Time
+	checkingEndTime      time.Time
 }
 
 type Attempt struct {
 	id             int64 `gorm:"primaryKey"`
 	pcmsId         string
-	score          string
+	score          int
 	outcome        string
 	status         string
-	participant    string
-	problemContest string
+	participant    int
+	problemContest int
 	time           int
 	language       string
 	source         string
@@ -31,8 +34,24 @@ type Attempt struct {
 func JobProcess(db *gorm.DB, jobId int64) {
 	var job AttemptsCheckJobs
 	db.First(&job, "id = ?", jobId)
+	var attemptLhs Attempt
+	db.First(&attemptLhs, "id = ?", job.attemptLhs)
+	var attemptRhs Attempt
+	db.First(&attemptRhs, "id = ?", job.attemptRhs)
 
-	// TODO: run comparing
+	sourceLhs, err := ioutil.ReadFile(attemptLhs.source)
+	if err != nil {
+		log.Printf("Failed to read a source file, msg: %v", err)
+	}
+	sourceRhs, err := ioutil.ReadFile(attemptRhs.source)
+	if err != nil {
+		log.Printf("Failed to read a source file, msg: %v", err)
+	}
+
+	tokensLhs := Tokenize(sourceLhs, attemptLhs.source)
+	tokensRhs := Tokenize(sourceRhs, attemptRhs.source)
+
+	job.scriptCheckingResult = SourcesCompare(tokensLhs, tokensRhs)
 
 	db.Save(&job)
 }
